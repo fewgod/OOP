cf22d10c0a187d0e092e793a14686f6db683d3a0
Few <phuklagod@hotmail.com>
2017-09-08 14:47:43 +0700

    add models.py

---
+++ models.py
@@ -0,0 +1,20 @@
+class World:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+ 
+        self.ship = Ship(100, 100)
+ 
+ 
+    def update(self, delta):
+        self.ship.update(delta)
+
+class Ship:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+ 
+    def update(self, delta):
+        if self.y > 600:
+            self.y = 0
+        self.y += 5
